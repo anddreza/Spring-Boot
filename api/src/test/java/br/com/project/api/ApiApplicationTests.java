e2aafeffc2a0cf3095b9eb58c50fca8acef92b74
Andreza <dedezasilva@live.com>
2023-09-18 11:36:02 -0300

    feat: tem que rodar a primeira vez para ele subir na 8080

---
+++ api/src/test/java/br/com/project/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.project.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
