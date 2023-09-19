63997f5102e6c64ed00f156f85c756e8c96146a4
Andreza <dedezasilva@live.com>
2023-09-19 11:54:20 -0300

    adição de novas anotations

--- api/src/main/java/br/com/project/api/model/Pessoa.java
+++ api/src/main/java/br/com/project/api/model/Pessoa.java
@@ -1,9 +1,30 @@
 package br.com.project.api.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//Especificar a criação da tabela 
+@Entity
+//Criar um outro nome de tabela, caso queira alterar o nome, caso quiser que o nome seja o mesmo da class então não necessário essa anotation
+@Table(name = "pessoas")
 public class Pessoa {
 	//Atributos
+	@Id //criação da PK
+	//gerar valores de maneira crescente
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
 	private String nome;
 	private int idade;
+
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
 	
 	public String getNome() {
 		return nome;
