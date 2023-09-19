9df88fb7a889271eb608af5d6ab7fee59f6ab487
Andreza <dedezasilva@live.com>
2023-09-19 14:51:07 -0300

    feat: alterado somente os comentários para ficar visivelmente compreensivo

--- api/src/main/java/br/com/project/api/model/Pessoa.java
+++ api/src/main/java/br/com/project/api/model/Pessoa.java
@@ -6,15 +6,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//Especificar a criação da tabela 
 @Entity
-//Criar um outro nome de tabela, caso queira alterar o nome, caso quiser que o nome seja o mesmo da class então não necessário essa anotation
 @Table(name = "pessoas")
 public class Pessoa {
 	//Atributos
-	@Id //criação da PK
-	//gerar valores de maneira crescente
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int codigo;
 	private String nome;
 	private int idade;
@@ -22,6 +19,7 @@ public class Pessoa {
 	public int getCodigo() {
 		return codigo;
 	}
+	
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
